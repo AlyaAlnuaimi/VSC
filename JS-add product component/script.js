98fd3d0992b7731a755b091840ec88f6c7490748
AlyaAlnuaimi <aliaalnuaimi111@gmail.com>
2022-11-24 15:42:41 +0400

    js add product

--- JS-add product component/script.js
+++ JS-add product component/script.js
@@ -2,7 +2,7 @@
 function addItems(){
     let product = document.getElementById('products');
     let product_name = document.getElementById('product-name');
-    var tr = document.createElement("tr");
+    let tr = document.createElement("tr");
     tr.setAttribute('id', product_name.value);
     tr.appendChild(document.createTextNode(product_name.value));
     product.appendChild(tr);
